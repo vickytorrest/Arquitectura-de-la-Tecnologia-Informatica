0d17048df567263259847e21f6521de920fe3a63
vickytorrest <mariatorres7@uca.edu.ar>
2025-04-03 18:39:16 -0300

    Add files via upload

---
+++ Clase 03_04 (1).py
@@ -0,0 +1,11 @@
+valor=input("Ingrese un valor")
+valor2=input("Ingrese valor 2")
+print(valor,valor2)
+
+if valor > valor2:
+    print("valor menor a valor2")
+elif valor <  valor2:
+    print("valor mayor a valor2")
+elif valor==valor2:
+    print("Son iguales")
+
---
+++ Clase 03_04.py
@@ -0,0 +1,45 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: 8'))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+edad=18=#indica la edad
+...
+Hola!!!
+...
+
+float=flotante  o real
+    precio=9.99
+    altura=1.75
+    
+cadena detexto=basestring
+nombre="Rafael"
+mensaje="Holaaaa"'Mundo?'
+
+Booleanos=Bool
+
+mayor_edad=True; mayor_edad=False
+
+if edad = 18
+if edad == 18
+
+a = b = c = 10
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
